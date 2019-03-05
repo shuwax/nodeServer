4754c4c70df0bfb70f002d4f12be76e0bff8c142
Sebastian Cytawa <sebastian.cytawa@edulab.io>
2019-03-05 13:26:15 +0100

    First step of verify content

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const { index } = require('./controller')
-
-router.get('/', index)
+const { token } = require('../../services/passport')
+router.get('/',
+  token({ required: true }),
+  index)
 
 module.exports = router
--- src/services/passport/index.js
+++ src/services/passport/index.js
@@ -1,8 +1,42 @@
 const passport = require('passport')
 const BearerStrategy = require('passport-http-bearer').Strategy
 const { BasicStrategy } = require('passport-http')
+const { ExtractJwt } = require('passport-jwt')
+const JwtStrategy = require('passport-jwt').Strategy
 const { MASTER_KEY, JWT_KEY } = require('../../constants').ENV_CONSTANTS
 const User = require('../../api/user/model')
+
+const token = ({ required, roles = User.roles } = {}) => (req, res, next) =>
+
+  passport.authenticate('token', { session: false }, (err, user, info) => {
+    console.log(info)
+    if (err || (required && !user) || (required && !~roles.indexOf(user.role))) {
+      return res.status(401).end()
+    }
+    req.logIn(user, { session: false }, (err) => {
+      if (err) return res.status(401).end()
+      next()
+    })
+  })(req, res, next)
+
+passport.use('token', new JwtStrategy({
+  secretOrKey: JWT_KEY,
+  jwtFromRequest: ExtractJwt.fromExtractors([
+    ExtractJwt.fromUrlQueryParameter('access_token'),
+    ExtractJwt.fromBodyField('access_token'),
+    ExtractJwt.fromAuthHeaderWithScheme('Bearer')
+  ])
+}, async ({ id }, done) => {
+  try {
+    const user = User.findById(id)
+    done(null, user)
+    return null
+  } catch (error) {
+    return done
+  }
+}))
+
+exports.token = token
 
 const password = () => (req, res, next) =>
   passport.authenticate('password', { session: false }, (err, user, info) => {
